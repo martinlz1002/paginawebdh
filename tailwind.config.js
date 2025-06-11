4d9164a6460b0f75f74102490e7ea762d266b0d8
martinlz1002 <33608068+martinlz1002@users.noreply.github.com>
2025-06-10 21:26:40 -0700

    muchas correcciones

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}"
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        softGreen: "#a8d5ba",
-        softPurple: "#c6b8e9"
-      }
-    }
+    extend: {}
   },
   plugins: []
 }
