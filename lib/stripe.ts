f7fadfa0d92bda5fb9952bbf7ad0c62d2d98c369
martinlz1002 <33608068+martinlz1002@users.noreply.github.com>
2025-06-28 16:22:57 -0700

    reparando webhook stripe2

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,8 @@
 import Stripe from "stripe";
-// @ts-ignore
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-05-28.basil",
-});
+
+export const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY!,
+  {
+    apiVersion: "2025-05-28.basil", // deja que Stripe use tu versión por defecto (2025-05-28.basil)
+  }
+);
